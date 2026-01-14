31178614b61bb4324aeed9ee6ecb4d67340231ef
Jack.Ball <jack.ball@ukhsa.gov.uk>
2026-01-14 14:16:02 +0000

    fix: update backend terraform bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-prod"
+    bucket         = "jackball-bucket/tf"
     key            = "glue/s3-copy/terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "terraform-locks"
